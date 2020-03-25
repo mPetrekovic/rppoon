c1b8b3e8d6a3321b7de4c4297aec3ae2d49192f6
mPetrekovic <62648565+mPetrekovic@users.noreply.github.com>
2020-03-25 19:13:14 +0100

    Add files via upload

---
+++ LV1/Note.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LV1
+{
+    class Note
+    {
+        private string mText;
+        private string mAuthor;
+        private int mPriority;
+        public Note()
+        {
+            mText = "Ovo je bilješka";
+            mAuthor = "Mihovil Petrekovic";
+            mPriority = 3;
+        }
+        public Note(string text, string author, int priority)
+        {
+            mText = text;
+            mAuthor = author;
+            mPriority = priority;
+        }
+        public Note(string author)
+        {
+            mText = "biljeska tri";
+            mAuthor = author;
+            mPriority = 3;
+        }
+        public void setText(string text)
+        {
+            mText = text;
+        }
+        public void setPriority(int priority)
+        {
+            mPriority = priority;
+        }
+        public string getText()
+        {
+            return mText;
+        }
+        public string getAuthor()
+        {
+            return mAuthor;
+        }
+        public string Text
+        {
+            get { return mText; }
+            set { mText = value; }
+        }
+        public string Author
+        {
+            get { return mAuthor; }
+            set { mAuthor = value; }
+        }
+        public int Priority
+        {
+            get { return mPriority; }
+            set { mPriority = value; }
+        }
+        public override string ToString()
+        {
+            return mText + "\n autor:" + mAuthor;
+        }
+    }
+}
---
+++ LV1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LV1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Note prva = new Note();
+            Note druga = new Note("Mr. Someone");
+            Note treca = new Note("Ovo je druga biljeska", "Mr. SomeoneElse", 3);
+            Console.WriteLine(prva.getText());
+            Console.WriteLine(prva.getAuthor());
+            Console.WriteLine(druga.getText());
+            Console.WriteLine(druga.getAuthor());
+            Console.WriteLine(treca.getText());
+            Console.WriteLine(treca.getAuthor());
+
+        }
+    }
+}
