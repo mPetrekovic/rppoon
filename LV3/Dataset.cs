a3014fe4dd66aa20a5435e6187534ca312d692a6
mPetrekovic <62648565+mPetrekovic@users.noreply.github.com>
2020-04-24 19:56:54 +0200

    Add files via upload

---
+++ LV3/Dataset.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3
+{
+    interface Prototype
+    {
+        Prototype Clone();
+    }
+    class Dataset : Prototype
+    {
+        private List<List<string>> data; //list of lists of strings 
+        public Dataset()
+        {
+            this.data = new List<List<string>>();
+        }
+        public Dataset(string filePath) : this()
+        {
+            this.LoadDataFromCSV(filePath);
+        }
+        public void LoadDataFromCSV(string filePath)
+        {
+            using (System.IO.StreamReader reader = new System.IO.StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    List<string> row = new List<string>();
+                    string[] items = line.Split(',');
+                    foreach (string item in items)
+                    {
+                        row.Add(item);
+                    }
+                    this.data.Add(row);
+                }
+            }
+        }
+        public IList<List<string>> GetData()
+        {
+            return new System.Collections.ObjectModel.ReadOnlyCollection<List<string>>(data);
+        }
+        public void ClearData() 
+        {
+            this.data.Clear();
+        }
+        public Prototype Clone()
+        {
+            private Dataset copy = new Dataset();
+            string line;
+            foreach(List<string> line in this.data)
+            {
+                List<string> row = new List<string>();
+                string items = line;
+                foreach (string item in items)
+                {
+                    row.Add(item); 
+                }
+                this.data.Add(row); 
+            }
+            return (Prototype)copy;
+        }
+    }
+}
---
+++ LV3/RandomMatrix.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LV3
+{
+    class RandomMatrix
+    {
+        private static RandomMatrix instance;
+        private Random[][] generator;
+        private RandomMatrix(int a,int b)
+        {
+            for (int i = 0; i < a; i++) 
+                for (int j = 0; j < b; j++) 
+            this.generator[i][j] = new Random(1); 
+        }
+        public static RandomMatrix GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RandomMatrix(1,1);            
+            }
+            return instance; 
+        }
+        public double[][] zadatakDva(int a,int b)  
+        {
+            private double matrica[a][b] = new double;
+            for (int i = 0; i<a; i++) 
+                for (int j = 0; j<b; j++) 
+                    matrica[i][j]=Random a.Next(1);
+            return matrica;
+        }
+    }
+}
