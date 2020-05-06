70be9d622c49e62988067421e9a1a9e186a30157
mPetrekovic <62648565+mPetrekovic@users.noreply.github.com>
2020-05-06 19:22:01 +0200

    Add files via upload

---
+++ LV5/Box.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lv_4
+{
+    class Box : IShipable
+
+    {
+        private List<IShipable> items;
+        private string name;
+        private double weight = 0;
+        public Box(string name)
+        {
+            this.items = new List<IShipable>();
+            this.name = name;
+        }
+        public void Add(IShipable item)
+        {
+            this.items.Add(item);
+            this.weight += item.Weight; 
+        }
+        public void Remove(IShipable item)
+        {
+            this.items.Remove(item);
+            this.weight -= item.Weight;
+        }
+        public double Price
+        {
+            get
+            {
+                double totalPrice = 0;
+                foreach (IShipable item in items)
+                {
+                    totalPrice += item.Price;
+                }
+                return totalPrice;
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return this.weight;
+            }
+        }
+        public string Description(int depth = 0)
+        {
+            StringBuilder builder =
+           new StringBuilder(new string(' ', depth) + this.name + "\n");
+            foreach (IShipable item in items)
+            {
+                builder.Append(item.Description(depth + 2)).Append("\n");
+            }
+            return builder.ToString();
+        }
+    }
+}
---
+++ LV5/ShipingService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lv_4
+{
+    class ShipingService
+    {
+        private double pricePerMass;
+        public ShipingService(double price)
+            {
+            this.pricePerMass = price;
+            }
+        public double shipingPrice(Box box)
+        {
+            return box.Weight * this.pricePerMass;
+        }
+    }
+}
